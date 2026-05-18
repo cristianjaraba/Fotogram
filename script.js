9676ccd1e5dfa06c7f26f08df48300c865ef8da1
Cristian Jaraba <cristianjaraba@hotmail.com>
2026-05-18 11:59:47 +0200

    Refactor dialog system: generate modal content dynamically and navigate photos by index

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-let myImgs = [
-  "anthony-x-graffiti-643065_1280.jpg",
-  "erikstrehl-building-6888533_1280.jpg",
-  "hesamhassani-bridge-7728877_1280.jpg",
-  "michael_kastelic-woman-7977101_1280.jpg",
-  "nicst-road-6975808_1280.jpg",
-  "noelsch-city-4468570_1280.jpg",
-  "peterdargatz-berlin-wall-50730_1280.jpg",
-  "st33lv0ll-berlin-4588073_1280.jpg",
-  "st33lv0ll-building-4679964_1280.jpg",
-  "st33lv0ll1-berlin-3958573_1280.jpg",
-  "wagneranne-berlin-5289802_1280.jpg",
-  "wal_172619-bridge-7770337_1280.jpg",
-  "wal_172619-city-9599967_1280.jpg",
-  "wal_172619-museum-5400973_1280.jpg",
-  "wal_172619-river-5887763_1280.jpg",
-  "wal_172619-train-7779221_1280.jpg",
-  "wal_172619-train-9596810_1280.jpg"
-];
-
 const fotosContainerRef = document.getElementById("fotos-container");
-const fotoMainRef = document.getElementById("main-foto");
-const fotoDialogRef = document.getElementById("dialog-foto");
 const dialogRef = document.getElementById("dialog");
-const closeDialogBtnRef = document.getElementById("close-dialog-btn");
-const fotoIndexRef = document.getElementById("foto-index");
-const fotoNameDisplayRef = document.getElementById("foto-name-display");
+let myImgs = [
+    "anthony-x-graffiti-643065_1280.jpg",
+    "erikstrehl-building-6888533_1280.jpg",
+    "hesamhassani-bridge-7728877_1280.jpg",
+    "michael_kastelic-woman-7977101_1280.jpg",
+    "nicst-road-6975808_1280.jpg",
+    "noelsch-city-4468570_1280.jpg",
+    "peterdargatz-berlin-wall-50730_1280.jpg",
+    "st33lv0ll-berlin-4588073_1280.jpg",
+    "st33lv0ll-building-4679964_1280.jpg",
+    "st33lv0ll1-berlin-3958573_1280.jpg",
+    "wagneranne-berlin-5289802_1280.jpg",
+    "wal_172619-bridge-7770337_1280.jpg",
+    "wal_172619-city-9599967_1280.jpg",
+    "wal_172619-museum-5400973_1280.jpg",
+    "wal_172619-river-5887763_1280.jpg",
+    "wal_172619-train-7779221_1280.jpg",
+    "wal_172619-train-9596810_1280.jpg"
+];
 
 // Load all images and add them to fotos-container div
 
@@ -33,51 +27,71 @@ for (let i = 0; i < myImgs.length; i++) {
 }
 
 function getNoteTemplate(index) {
-    return `<img id="main-foto" onclick="openDialog(event)" src="./ASSETS/images/${myImgs[index]}">`;
+
+    return `<img id="main-foto" onclick="openDialog(${index})" src="./ASSETS/images/${myImgs[index]}">`;
 }
 
 // Dialog functions
 
-function openDialog(e) {
+function openDialog(i) {
+
+    dialogRef.innerHTML = `
+     <header>
+            <button onclick="closeDialog()" id="close-dialog-btn">
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_63_506" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="4" y="4" width="24" height="24">
+                <rect x="4" y="4" width="24" height="24" fill="#D9D9D9"/>
+                </mask>
+                <g mask="url(#mask0_63_506)">
+                <path d="M16 17.4L11.1 22.3C10.9167 22.4833 10.6834 22.575 10.4 22.575C10.1167 22.575 9.88338 22.4833 9.70005 22.3C9.51672 22.1167 9.42505 21.8833 9.42505 21.6C9.42505 21.3167 9.51672 21.0833 9.70005 20.9L14.6 16L9.70005 11.1C9.51672 10.9167 9.42505 10.6833 9.42505 10.4C9.42505 10.1167 9.51672 9.88334 9.70005 9.7C9.88338 9.51667 10.1167 9.425 10.4 9.425C10.6834 9.425 10.9167 9.51667 11.1 9.7L16 14.6L20.9 9.7C21.0834 9.51667 21.3167 9.425 21.6 9.425C21.8834 9.425 22.1167 9.51667 22.3 9.7C22.4834 9.88334 22.575 10.1167 22.575 10.4C22.575 10.6833 22.4834 10.9167 22.3 11.1L17.4 16L22.3 20.9C22.4834 21.0833 22.575 21.3167 22.575 21.6C22.575 21.8833 22.4834 22.1167 22.3 22.3C22.1167 22.4833 21.8834 22.575 21.6 22.575C21.3167 22.575 21.0834 22.4833 20.9 22.3L16 17.4Z" fill="white"/>
+                </g>
+                </svg>
+            </button>
+            <p>${myImgs[i]}</p>
+        </header>
+
+        <section id="section">
+            <img id="dialog-foto" src="./ASSETS/images/${myImgs[i]}" alt="">
+        </section>
+
+        <footer>
+            <button onclick="showPreviousFoto(${i})" id="previous-btn">
+                <svg width="24" height="24" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.77824 18.6777C9.16877 19.0682 9.80193 19.0682 10.1925 18.6777C10.583 18.2871 10.583 17.654 10.1925 17.2634L3.41373 10.4847L20.5811 10.4847C21.1333 10.4847 21.5811 10.037 21.5811 9.48471C21.5811 8.93243 21.1333 8.48471 20.5811 8.48471L3.41484 8.48471L10.1925 1.7071C10.583 1.31657 10.583 0.683406 10.1925 0.29288C9.80194 -0.0976439 9.16877 -0.097644 8.77825 0.29288L0.319655 8.75147C0.122815 8.9341 -0.000365356 9.19502 -0.000365382 9.48471C-0.000365398 9.67361 0.0520123 9.85028 0.143037 10.001C0.184148 10.0692 0.234124 10.1335 0.292966 10.1924L8.77824 18.6777Z" fill="#FD5B4F"/>
+            </svg>
+            </button>
+            <p>${myImgs.indexOf(myImgs[i]) + 1}/${myImgs.length}</p>
+            <button onclick="showNextFoto(${i})" id="next-btn">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.0118 2.80754C13.6213 2.41702 12.9881 2.41702 12.5976 2.80754C12.2071 3.19807 12.2071 3.83123 12.5976 4.22176L19.3763 11.0005H2.20898C1.6567 11.0005 1.20898 11.4482 1.20898 12.0005C1.20898 12.5528 1.6567 13.0005 2.20898 13.0005H19.3752L12.5976 19.7781C12.2071 20.1686 12.2071 20.8018 12.5976 21.1923C12.9881 21.5828 13.6213 21.5828 14.0118 21.1923L22.4704 12.7337C22.6672 12.5511 22.7904 12.2902 22.7904 12.0005C22.7904 11.8116 22.738 11.6349 22.647 11.4842C22.6059 11.416 22.5559 11.3517 22.4971 11.2928L14.0118 2.80754Z" fill="#FD5B4F"/>
+                </svg>
+            </button>
+        </footer>`;
+
     dialogRef.showModal();
-    fotoDialogRef.src = e.target.src;
-    showFotoName()
-    showFotoIndex();
 }
 
 function closeDialog() {
+
     dialogRef.close();
 }
 
-function showFotoIndex() {
-    fotoIndexRef.innerHTML = `${myImgs.indexOf(fotoDialogRef.src.slice(36))+1}/${myImgs.length}`;
+function showPreviousFoto(i) {
+
+    if (i === 0) {
+        openDialog(myImgs.length - 1);
+    }
+    else {
+        openDialog(i - 1);
+    }
 }
 
-function showPreviousFoto() {
-    let i = myImgs.indexOf(fotoDialogRef.src.slice(36));
-    
-    if(i === 0){
-        fotoDialogRef.src = "./ASSETS/images/" + myImgs[myImgs.length - 1];
-    }
-    else{
-        fotoDialogRef.src = "./ASSETS/images/" + myImgs[i-1];
-    }
-    showFotoName()
-    showFotoIndex();
-}
+function showNextFoto(i) {
 
-function showNextFoto() {
-    let i = myImgs.indexOf(fotoDialogRef.src.slice(36));
-    
-    if(i === myImgs.length - 1){
-        fotoDialogRef.src = "./ASSETS/images/" + myImgs[0];
+    if (i === myImgs.length - 1) {
+        openDialog(0);
     }
-    else{
-        fotoDialogRef.src = "./ASSETS/images/" + myImgs[i+1];
+    else {
+        openDialog(i + 1);
     }
-    showFotoName()
-    showFotoIndex();
-}
-function showFotoName() {
-    fotoNameDisplayRef.innerHTML = fotoDialogRef.src.slice(36);
 }
