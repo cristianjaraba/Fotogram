95fda27cff341a108b11f833d8e9342907aa7fc8
Cristian Jaraba <cristianjaraba@hotmail.com>
2026-05-19 20:17:47 +0200

    feat: Add play/pause controls and auto‑pause slideshow on dialog close

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let myImgs = [
     ["noelsch-city-4468570_1280.jpg", "Berlin city view"],
     ["peterdargatz-berlin-wall-50730_1280.jpg", "Berlin Wall artwork"],
     ["st33lv0ll-berlin-4588073_1280.jpg", "Berlin street scene"],
-    ["st33lv0ll-building-4679964_1280.jpg", "Old building in Berlin"],
+    ["st33lv0ll-building-4679964_1280.jpg", "Modern building in Berlin"],
     ["st33lv0ll1-berlin-3958573_1280.jpg", "Berlin architecture"],
     ["wagneranne-berlin-5289802_1280.jpg", "Berlin landmark"],
     ["wal_172619-bridge-7770337_1280.jpg", "Bridge over the river in Berlin"],
@@ -19,7 +19,7 @@ let myImgs = [
     ["wal_172619-train-7779221_1280.jpg", "Train in Berlin"],
     ["wal_172619-train-9596810_1280.jpg", "Berlin train passing by"]
 ];
-
+let myInterval;
 
 // Load all images and add them to photos-container div
 
@@ -63,6 +63,18 @@ function getDialogHTML(i) {
         </div>
 
         <footer class="dialog-footer">
+
+            <div class="controls">
+                <button type="button" onclick="playSlideshow(${i})">
+                    <img src="./ASSETS/icons/play.svg" alt="Play slideshow">
+                </button>
+
+                <button type="button" onclick="pauseSlideshow()">
+                    <img src="./ASSETS/icons/pause.svg" alt="Pause slideshow">
+                </button>
+            </div>
+
+            <div class="arrays-index-container">
             <button type="button" onclick="showPreviousPhoto(${i})" id="previous-btn" aria-label="Previous photo">
                 <svg aria-hidden="true" width="24" height="24" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M8.77824 18.6777C9.16877 19.0682 9.80193 19.0682 10.1925 18.6777C10.583 18.2871 10.583 17.654 10.1925 17.2634L3.41373 10.4847L20.5811 10.4847C21.1333 10.4847 21.5811 10.037 21.5811 9.48471C21.5811 8.93243 21.1333 8.48471 20.5811 8.48471L3.41484 8.48471L10.1925 1.7071C10.583 1.31657 10.583 0.683406 10.1925 0.29288C9.80194 -0.0976439 9.16877 -0.097644 8.77825 0.29288L0.319655 8.75147C0.122815 8.9341 -0.000365356 9.19502 -0.000365382 9.48471C-0.000365398 9.67361 0.0520123 9.85028 0.143037 10.001C0.184148 10.0692 0.234124 10.1335 0.292966 10.1924L8.77824 18.6777Z" fill="#FD5B4F"/>
@@ -74,6 +86,7 @@ function getDialogHTML(i) {
                 <path d="M14.0118 2.80754C13.6213 2.41702 12.9881 2.41702 12.5976 2.80754C12.2071 3.19807 12.2071 3.83123 12.5976 4.22176L19.3763 11.0005H2.20898C1.6567 11.0005 1.20898 11.4482 1.20898 12.0005C1.20898 12.5528 1.6567 13.0005 2.20898 13.0005H19.3752L12.5976 19.7781C12.2071 20.1686 12.2071 20.8018 12.5976 21.1923C12.9881 21.5828 13.6213 21.5828 14.0118 21.1923L22.4704 12.7337C22.6672 12.5511 22.7904 12.2902 22.7904 12.0005C22.7904 11.8116 22.738 11.6349 22.647 11.4842C22.6059 11.416 22.5559 11.3517 22.4971 11.2928L14.0118 2.80754Z" fill="#FD5B4F"/>
                 </svg>
             </button>
+            </div>
         </footer>`;
 }
 
@@ -85,7 +98,7 @@ function openDialog(i) {
 }
 
 function closeDialog() {
-
+    pauseSlideshow();
     dialogRef.close();
 }
 
@@ -108,3 +121,25 @@ function showNextPhoto(i) {
         openDialog(i + 1);
     }
 }
+
+function playSlideshow(i) {
+    myInterval = setInterval(function () {
+        showNextPhoto(i);
+        if (i === myImgs.length - 1) {
+            i = 0;
+        } else {
+            i++;
+        }     
+    }, 1000);
+}
+
+function pauseSlideshow() {
+    clearInterval(myInterval);
+}
+
+dialogRef.addEventListener("close", () => {
+  pauseSlideshow();
+});
+
+
+
